e6a821a5f213c6a02b22405b0aeb4ab043df3cbc
Jammiusis <tiagu16@hotmail.com>
2021-06-15 15:44:48 +0100

    Movimentação do Personagem
    -Andar
    -Saltar
    -Correr
    -Crouch Walk
    -Hp
    -Esc to Pause

---
+++ TBA-MasterD/Assets/Scripts/Tiago/cameraRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotation : MonoBehaviour
+{
+    //movimentos do Rato
+    private float mouseX;
+    private float mouseY;
+   [SerializeField] public float mouseSensitivy = 100f;
+
+    //corpo do jogador
+   [SerializeField] public Transform playerBody;
+    private float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+   
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivy * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivy * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+    }
+}
---
+++ TBA-MasterD/Assets/Scripts/Tiago/playerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    //velocidades de movimento
+    public int moveSpeed;
+    public int sprintSpeed;
+    public int crouchSpeed;
+    Vector3 velocity;
+    public float gravity;
+    public bool isRunning = false;
+    public bool isCrouched = false;
+
+    //extras
+    public CharacterController controller;
+    public Camera cam;
+    public float crouchHeight;
+
+    //Info Sobre Player
+    public int vida = 100;
+
+
+    //saltar
+    private bool isGrounded = true;
+    public float jumpHeight;
+
+
+    void Update()
+    {
+        //andar
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (isRunning == false && isCrouched == false)
+        {
+            controller.Move(move * moveSpeed * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        //correr
+        if (Input.GetKey(KeyCode.LeftShift) && isCrouched == false)
+        {
+            controller.Move(move * sprintSpeed * Time.deltaTime);
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+
+        //saltar
+        if (Input.GetKeyDown("space") && isGrounded == true)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            isGrounded = false;
+            StartCoroutine(jumpingCoolDown());
+        }
+        IEnumerator jumpingCoolDown()
+        {
+            yield return new WaitForSeconds(0.5f);
+            isGrounded = true;
+        }
+
+        //crouch
+       
+        if(Input.GetKey(KeyCode.LeftControl) && isRunning == false)
+        {
+            controller.Move(move * crouchSpeed * Time.deltaTime);
+            cam.transform.position = new Vector3(transform.position.x, transform.position.y - crouchHeight, transform.position.z);
+            isCrouched = true;
+            
+        }
+        else
+        {
+            cam.transform.position = new Vector3(transform.position.x, transform.position.y + crouchHeight, transform.position.z);
+            isCrouched = false;
+        }
+
+        //Pause
+        if (Input.GetKeyDown("escape"))
+        {
+            Debug.Log("Game is Paused");
+        }
+       
+    }
+}
