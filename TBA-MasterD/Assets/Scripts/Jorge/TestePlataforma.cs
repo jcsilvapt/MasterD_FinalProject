976f0e030d7086fc0c9e43c3f19989d66a4a2883
Jorge Silva <jorgemcs14@gmail.com>
2021-06-25 18:45:34 +0100

    Map & Lightning Update
    - Fixed Materials and Lightning;
    - Added some props;
    - Added a Horizontal Crane;
    - Added a Script to the crane that allow the player to go with it ....;
    - Multiple Fix on all meshes.

---
+++ TBA-MasterD/Assets/Scripts/Jorge/TestePlataforma.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestePlataforma : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            Debug.Log("Tenho o jogador em cima de mim");
+            other.gameObject.transform.parent = transform;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            other.gameObject.transform.parent = null;
+        }
+    }
+}
