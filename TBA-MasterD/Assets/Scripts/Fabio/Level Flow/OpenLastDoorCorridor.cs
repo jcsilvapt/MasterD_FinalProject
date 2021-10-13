a88b445fa4b549ea42adb06820c5f81e03a566d6
DarkKool <43683250+DarkKool@users.noreply.github.com>
2021-10-13 04:40:03 +0100

    Added a Door Opener to Second Checkpoint in Second Level
    Now, if the player loads the game and pops up in the second checkpoint, he can go back.
    This allows him to visit the EasterEggRoom in case he leaves before he visits it.

---
+++ TBA-MasterD/Assets/Scripts/Fabio/Level Flow/OpenLastDoorCorridor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLastDoorCorridor : MonoBehaviour
+{
+    [SerializeField] private DoorController door;
+
+    private bool canActive;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!canActive)
+        {
+            if (other.CompareTag("Player"))
+            {
+                door.LockMode(false);
+                canActive = true;
+            }
+        }
+    }
+}
